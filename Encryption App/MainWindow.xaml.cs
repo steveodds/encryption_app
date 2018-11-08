1589ffe822d90ca259b29f9af24df15070bfaae6
Steve Odds <steveremdi@gmail.com>
2018-11-08 17:56:07 +0300

    Add project files.

---
+++ Encryption App/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Security.Cryptography;
+using System.IO;
+using Microsoft.VisualBasic;
+
+namespace Encryption_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public static class Encrypt
+    {
+        // This size of the IV (in bytes) must = (keysize / 8).  Default keysize is 256, so the IV must be
+        // 32 bytes long.  Using a 16 character string here gives us 32 bytes when converted to a byte array.
+        private const string initVector = "pemgail9uzpgzl88";
+        // This constant is used to determine the keysize of the encryption algorithm
+        private const int keysize = 256;
+        //Encrypt
+        public static string EncryptString(string plainText, string passPhrase)
+        {
+            byte[] initVectorBytes = Encoding.UTF8.GetBytes(initVector);
+            byte[] plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            PasswordDeriveBytes password = new PasswordDeriveBytes(passPhrase, null);
+            byte[] keyBytes = password.GetBytes(keysize / 8);
+            RijndaelManaged symmetricKey = new RijndaelManaged();
+            symmetricKey.Mode = CipherMode.CBC;
+            ICryptoTransform encryptor = symmetricKey.CreateEncryptor(keyBytes, initVectorBytes);
+            MemoryStream memoryStream = new MemoryStream();
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, encryptor, CryptoStreamMode.Write);
+            cryptoStream.Write(plainTextBytes, 0, plainTextBytes.Length);
+            cryptoStream.FlushFinalBlock();
+            byte[] cipherTextBytes = memoryStream.ToArray();
+            memoryStream.Close();
+            cryptoStream.Close();
+            return Convert.ToBase64String(cipherTextBytes);
+        }
+        //Decrypt
+        public static string DecryptString(string cipherText, string passPhrase)
+        {
+            byte[] initVectorBytes = Encoding.UTF8.GetBytes(initVector);
+            byte[] cipherTextBytes = Convert.FromBase64String(cipherText);
+            PasswordDeriveBytes password = new PasswordDeriveBytes(passPhrase, null);
+            byte[] keyBytes = password.GetBytes(keysize / 8);
+            RijndaelManaged symmetricKey = new RijndaelManaged();
+            symmetricKey.Mode = CipherMode.CBC;
+            ICryptoTransform decryptor = symmetricKey.CreateDecryptor(keyBytes, initVectorBytes);
+            MemoryStream memoryStream = new MemoryStream(cipherTextBytes);
+            CryptoStream cryptoStream = new CryptoStream(memoryStream, decryptor, CryptoStreamMode.Read);
+            byte[] plainTextBytes = new byte[cipherTextBytes.Length];
+            int decryptedByteCount = cryptoStream.Read(plainTextBytes, 0, plainTextBytes.Length);
+            memoryStream.Close();
+            cryptoStream.Close();
+            return Encoding.UTF8.GetString(plainTextBytes, 0, decryptedByteCount);
+        }
+    }
+    public partial class MainWindow : Window
+    {
+        public string keyphrase;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string TextEncrypt, FinalEncrypt;
+            TextEncrypt = txtTarget.Text;
+            keyphrase = Microsoft.VisualBasic.Interaction.InputBox("Enter Key Phrase", "Key Phrase", "password", -1, -1);
+            FinalEncrypt = Encrypt.EncryptString(TextEncrypt, keyphrase);
+            txtTarget.Text = String.Empty;
+            txtTarget.Text = FinalEncrypt;
+            System.Windows.Clipboard.SetText(FinalEncrypt);
+            MessageBox.Show("The encrypted text has been generated and copied to the clipboard.","Success!",MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            string EncryptedText, DecryptedText;
+            EncryptedText = txtTarget.Text;
+            keyphrase = Microsoft.VisualBasic.Interaction.InputBox("Enter Key Phrase", "Key Phrase", "password", -1, -1);
+            DecryptedText = Encrypt.DecryptString(EncryptedText, keyphrase);
+            txtTarget.Text = String.Empty;
+            txtTarget.Text = DecryptedText;
+            Clipboard.SetText(DecryptedText);
+            MessageBox.Show("The text has been decrypted and copied to the clipboard.", "Success!", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+    }
+}
